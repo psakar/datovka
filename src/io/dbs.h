3eae70b35992e9b09a9d0923c253e2ff41afe400
Karel Slany <karel.slany@nic.cz>
2014-04-10 15:03:48 +0200

    Added missing file.

---
+++ src/io/dbs.h
@@ -0,0 +1,18 @@
+
+
+#ifndef _DBS_H_
+#define _DBS_H_
+
+
+/*!
+ * @brief Used datatypes in databases.
+ */
+typedef enum {
+	INTEGER = 1,
+	TEXT,
+	BOOLEAN,
+	DATETIME
+} dbEntryType;
+
+
+#endif /* _DBS_H_ */
