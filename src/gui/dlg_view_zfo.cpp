e1132f8f0f6d5abf921228ab68f99818aedbfe6d
Karel Slany <karel.slany@nic.cz>
2014-06-26 16:52:57 +0200

    Showing attachments on ZFO files.

--- src/gui/dlg_view_zfo.cpp
+++ src/gui/dlg_view_zfo.cpp
@@ -1,7 +1,148 @@
 
 
+#include <QAbstractTableModel>
+#include <QDialog>
+
 #include "src/gui/dlg_view_zfo.h"
 #include "src/io/dbs.h"
+
+
+#define COL_NUM 2
+
+
+const QVector<QString> AttachmentModel::m_headerLabels = {
+QObject::tr("File name"),
+QObject::tr("Size")
+};
+
+
+/* ========================================================================= */
+/*
+ * Constructor.
+ */
+AttachmentModel::AttachmentModel(QObject *parent)
+/* ========================================================================= */
+    : QAbstractTableModel(parent),
+    m_docs()
+{
+}
+
+
+/* ========================================================================= */
+/*
+ * Destructor.
+ */
+AttachmentModel::~AttachmentModel(void)
+/* ========================================================================= */
+{
+}
+
+
+/* ========================================================================= */
+/*
+ * Returns row count.
+ */
+int AttachmentModel::rowCount(const QModelIndex &parent) const
+/* ========================================================================= */
+{
+	return m_docs.size();
+}
+
+
+/* ========================================================================= */
+/*
+ * Returns column count.
+ */
+int AttachmentModel::columnCount(const QModelIndex &parent) const
+/* ========================================================================= */
+{
+	return COL_NUM;
+}
+
+
+/* ========================================================================= */
+/*
+ * Returns data.
+ */
+QVariant AttachmentModel::data(const QModelIndex &index, int role) const
+/* ========================================================================= */
+{
+	int row, col;
+
+	switch (role) {
+	case Qt::DisplayRole:
+		row = index.row();
+		col = index.column();
+		Q_ASSERT(row < m_docs.size());
+		Q_ASSERT(col < COL_NUM);
+
+		if (0 == col) {
+			/* File name. */
+			return QString(m_docs[row]->dmFileDescr);
+		} else if (1 == col) {
+			/* File size. */
+			return QString::number(m_docs[row]->data_length);
+		}
+
+		return QVariant();
+		break;
+	case Qt::TextAlignmentRole:
+		return Qt::AlignLeft;
+		break;
+	default:
+		return QVariant();
+		break;
+	}
+}
+
+
+/* ========================================================================= */
+/*
+ * Returns header data.
+ */
+QVariant AttachmentModel::headerData(int section,
+    Qt::Orientation orientation, int role) const
+/* ========================================================================= */
+{
+	(void) orientation; /* Unused. */
+
+	if (role != Qt::DisplayRole) {
+		return QVariant();
+	}
+	return m_headerLabels[section];
+}
+
+
+/* ========================================================================= */
+/*
+ * Set attachment model according to message content.
+ */
+bool AttachmentModel::setModelData(const isds_message *message)
+/* ========================================================================= */
+{
+	const struct isds_list *docListItem;
+
+	m_docs.clear();
+
+	Q_ASSERT(NULL != message);
+	if (NULL == message) {
+		return false;
+	}
+
+	docListItem = message->documents;
+	Q_ASSERT(NULL != docListItem);
+	if (NULL == docListItem) {
+		return false;
+	}
+
+	while (NULL != docListItem) {
+		Q_ASSERT(NULL != docListItem->data);
+		m_docs.append((struct isds_document *) docListItem->data);
+		docListItem = docListItem->next;
+	}
+
+	return true;
+}
 
 
 /* ========================================================================= */
@@ -11,14 +152,23 @@
 DlgViewZfo::DlgViewZfo(const isds_message *isdsMsg, QWidget *parent)
 /* ========================================================================= */
     : QDialog(parent),
-    m_message(isdsMsg)
+    m_message(isdsMsg),
+    m_attachmentModel(0)
 {
 	setupUi(this);
 
+	/* TODO -- Adjust splitter sizes. */
+
 	Q_ASSERT(NULL != m_message);
 
-	envelopeTextEdit->setHtml(descriptionHtml());
+	m_attachmentModel.setModelData(isdsMsg);
+
+	envelopeTextEdit->setHtml(
+	    descriptionHtml(m_attachmentModel.m_docs.size()));
 	envelopeTextEdit->setReadOnly(true);
+
+	attachmentTable->setModel(&m_attachmentModel);
+	attachmentTable->resizeColumnToContents(0);
 }
 
 
@@ -26,7 +176,7 @@ DlgViewZfo::DlgViewZfo(const isds_message *isdsMsg, QWidget *parent)
 /*
  * Generate description from supplied message.
  */
-QString DlgViewZfo::descriptionHtml(void)
+QString DlgViewZfo::descriptionHtml(int attachmentCount)
 /* ========================================================================= */
 {
 	const isds_envelope *envelope;
@@ -75,7 +225,8 @@ QString DlgViewZfo::descriptionHtml(void)
 	        *(envelope->dmMessageStatus))) + " -- " +
 	    msgStatusToText(convertHexToDecIndex(
 	        *(envelope->dmMessageStatus))));
-	html += strongAccountInfoLine(tr("Attachments"), "TODO");
+	html += strongAccountInfoLine(tr("Attachments"),
+	    QString::number(attachmentCount));
 
 	html += "<h3>" + tr("Signature") + "</h3>";
 
--- src/gui/dlg_view_zfo.h
+++ src/gui/dlg_view_zfo.h
@@ -4,13 +4,77 @@
 #define _DLG_VIEW_ZFO_H_
 
 
+#include <QAbstractTableModel>
 #include <QDialog>
 
 #include "src/common.h"
 #include "src/io/isds_sessions.h"
 #include "ui_dlg_view_zfo.h"
 
+/*!
+ * @brief Attachment table model.
+ */
+class AttachmentModel : public QAbstractTableModel {
+	Q_OBJECT /* Not supported for nested classes. */
 
+	friend class DlgViewZfo;
+public:
+	/*!
+	 * @brief Constructor.
+	 */
+	AttachmentModel(QObject *parent = 0);
+
+	/*!
+	 * @brief Destructor.
+	 */
+	virtual
+	~AttachmentModel(void);
+
+	/*!
+	 * @brief Returns row count.
+	 */
+	virtual
+	int rowCount(const QModelIndex &parent = QModelIndex()) const;
+
+	/*!
+	 * @brief Returns column count.
+	 */
+	virtual
+	int columnCount(const QModelIndex &parent = QModelIndex()) const;
+
+	/*!
+	 * @brief Returns data.
+	 */
+	virtual
+	QVariant data(const QModelIndex &index,
+	    int role = Qt::DisplayRole) const;
+
+	/*!
+	 * @brief Returns header data.
+	 */
+	virtual
+	QVariant headerData(int section, Qt::Orientation orientation,
+	    int role) const;
+
+private:
+	/*!
+	 * @brief Set attachment model according to message content.
+	 *
+	 * @param[in] message Pointer to ISDS message.
+	 * @return True on success.
+	 */
+	bool setModelData(const isds_message *message);
+
+	static
+	const QVector<QString> m_headerLabels; /*!< Header labels. */
+
+	QVector<const struct isds_document *> m_docs; /*!< Pointers. */
+};
+
+
+/*!
+ * @brief Dialog for ZFO content viewing.
+ */
 class DlgViewZfo : public QDialog, public Ui::ViewZfo {
 	Q_OBJECT
 
@@ -32,11 +96,13 @@ private:
 	/*!
 	 * @brief Generate description from supplied message.
 	 *
+	 * @param[in] attachmentCount Number of attached files.
 	 * @return String containing description in HTML format.
 	 */
-	QString descriptionHtml(void);
+	QString descriptionHtml(int attachmentCount);
 
-	const isds_message *m_message;
+	const isds_message *m_message; /*!< ISDS message pointer copy. */
+	AttachmentModel m_attachmentModel; /*!< Attachment model. */
 };
 
 #endif /* _DLG_VIEW_ZFO_H_ */
