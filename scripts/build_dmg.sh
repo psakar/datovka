c8c0f804badd3c911bcdd9f2bd8dc6187d3eddc3
Karel Slany <karel.slany@nic.cz>
2014-11-26 18:37:23 +0100

    Added an automatic DPI fix into OS X dmg script.
    The sleep time-out has been increased to 10.

--- scripts/build_dmg.sh
+++ scripts/build_dmg.sh
@@ -26,7 +26,7 @@ hdiutil create \
 DEVICE=$(hdiutil attach -readwrite -noverify -noautoopen "${DMG_TMP_FILE}" | \
          egrep '^/dev/' | sed 1q | awk '{print $1}')
 
-sleep 5
+sleep 10
 
 echo $DEVICE
 VOLUME_DIR="/Volumes/${TITLE}"
@@ -47,6 +47,22 @@ if [ ! -f "${BG_SRC}" ]; then
 fi
 echo cp "${BG_SRC}" "${VOLUME_DIR}/.background/${backgroundPictureName}"
 cp "${BG_SRC}" "${VOLUME_DIR}/.background/${backgroundPictureName}"
+
+# Check the background image DPI and fix it if it is not 72x72
+DMG_BACKGROUND_IMG="${VOLUME_DIR}/.background/${backgroundPictureName}"
+_BACKGROUND_IMAGE_DPI_H=`sips -g dpiHeight "${DMG_BACKGROUND_IMG}" | grep -Eo '[0-9]+\.[0-9]+'`
+_BACKGROUND_IMAGE_DPI_W=`sips -g dpiWidth "${DMG_BACKGROUND_IMG}" | grep -Eo '[0-9]+\.[0-9]+'`
+
+if [ $(echo " $_BACKGROUND_IMAGE_DPI_H != 72.0 " | bc) -eq 1 -o $(echo " $_BACKGROUND_IMAGE_DPI_W != 72.0 " | bc) -eq 1 ]; then
+echo "WARNING: The background image's DPI is not 72. This will result in distorted backgrounds on Mac OS X 10.7+. I will convert it to 72 DPI for you."
+
+_DMG_BACKGROUND_TMP="${DMG_BACKGROUND_IMG%.*}"_dpifix."${DMG_BACKGROUND_IMG##*.}"
+
+sips -s dpiWidth 72 -s dpiHeight 72 "${DMG_BACKGROUND_IMG}" --out "${_DMG_BACKGROUND_TMP}"
+
+#DMG_BACKGROUND_IMG="${_DMG_BACKGROUND_TMP}"
+mv "${_DMG_BACKGROUND_TMP}" "${DMG_BACKGROUND_IMG}"
+fi
 
 applicationName="datovka"
 
