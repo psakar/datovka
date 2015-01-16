965ff56d90f4069426ea106ebc1d3fcedffb9bbf
Karel Slany <karel.slany@nic.cz>
2015-01-16 11:12:38 +0100

    Updated dependent libraries in mingw build script.
    Also building from libisds archive is possible as upstream
    now optionally contains OpenSSL back-end.

--- scripts/build_libisds_mingw.sh
+++ scripts/build_libisds_mingw.sh
@@ -27,25 +27,26 @@ X86_MINGW_LD=${X86_MINGV_HOST}-ld
 X86_MINGW_STRIP=${X86_MINGV_HOST}-strip
 X86_MINGW_RANLIB=${X86_MINGV_HOST}-ranlib
 
-ZLIB_ARCHIVE=zlib-1.2.8.tar.xz
-EXPAT_ARCHIVE=expat-2.1.0.tar.gz
-LIBTOOL_ARCHIVE=libtool-2.4.3.tar.xz
+ZLIB_ARCHIVE="zlib-1.2.8.tar.xz"
+EXPAT_ARCHIVE="expat-2.1.0.tar.gz"
+LIBTOOL_ARCHIVE="libtool-2.4.4.tar.xz"
 
-LIBICONV_ARCHIVE=libiconv-1.14.tar.gz
-LIBXML2_ARCHIVE=libxml2-2.9.2.tar.gz
-GETTEXT_ARCHIVE=gettext-0.19.3.tar.xz
+LIBICONV_ARCHIVE="libiconv-1.14.tar.gz"
+LIBXML2_ARCHIVE="libxml2-2.9.2.tar.gz"
+GETTEXT_ARCHIVE="gettext-0.19.4.tar.xz"
 
-LIBCURL_ARCHIVE=curl-7.39.0.tar.gz
-OPENSSL_ARCHIVE=openssl-1.0.1j.tar.gz
+LIBCURL_ARCHIVE="curl-7.40.0.tar.gz"
+OPENSSL_ARCHIVE="openssl-1.0.1l.tar.gz"
 
-LIBISDS_GIT="https://gitlab.labs.nic.cz/kslany/libisds.git"
-LIBISDS_BRANCH="feature-openssl"
+LIBISDS_ARCHIVE="libisds-0.10.tar.xz"
+#LIBISDS_GIT="https://gitlab.labs.nic.cz/kslany/libisds.git"
+#LIBISDS_BRANCH="feature-openssl" # Use master.
 
 
 if [ ! -z "${ZLIB_ARCHIVE}" ]; then
 	ARCHIVE="${SRCDIR}/${ZLIB_ARCHIVE}"
 	if [ ! -f "${ARCHIVE}" ]; then
-		echo "Missing ${ARCHIVE}"
+		echo "Missing ${ARCHIVE}" >&2
 		exit 1
 	fi
 	# zlib
@@ -60,7 +61,7 @@ fi
 if [ ! -z "${EXPAT_ARCHIVE}" ]; then
 	ARCHIVE="${SRCDIR}/${EXPAT_ARCHIVE}"
 	if [ ! -f "${ARCHIVE}" ]; then
-		echo "Missing ${ARCHIVE}"
+		echo "Missing ${ARCHIVE}" >&2
 		exit 1
 	fi
 	# expat
@@ -76,7 +77,7 @@ fi
 if [ ! -z "${LIBTOOL_ARCHIVE}" ]; then
 	ARCHIVE="${SRCDIR}/${LIBTOOL_ARCHIVE}"
 	if [ ! -f "${ARCHIVE}" ]; then
-		echo "Missing ${ARCHIVE}"
+		echo "Missing ${ARCHIVE}" >&2
 		exit 1
 	fi
 	# libtool
@@ -92,7 +93,7 @@ fi
 if [ ! -z "${LIBICONV_ARCHIVE}" ]; then
 	ARCHIVE="${SRCDIR}/${LIBICONV_ARCHIVE}"
 	if [ ! -f "${ARCHIVE}" ]; then
-		echo "Missing ${ARCHIVE}"
+		echo "Missing ${ARCHIVE}" >&2
 		exit 1
 	fi
 	# libiconv
@@ -109,7 +110,7 @@ fi
 if [ ! -z "${LIBXML2_ARCHIVE}" ]; then
 	ARCHIVE="${SRCDIR}/${LIBXML2_ARCHIVE}"
 	if [ ! -f "${ARCHIVE}" ]; then
-		echo "Missing ${ARCHIVE}"
+		echo "Missing ${ARCHIVE}" >&2
 		exit 1
 	fi
 	# libxml2
@@ -126,7 +127,7 @@ fi
 if [ ! -z "${GETTEXT_ARCHIVE}" ]; then
 	ARCHIVE="${SRCDIR}/${GETTEXT_ARCHIVE}"
 	if [ ! -f "${ARCHIVE}" ]; then
-		echo "Missing ${ARCHIVE}"
+		echo "Missing ${ARCHIVE}" >&2
 		exit 1
 	fi
 	# gettext
@@ -143,7 +144,7 @@ fi
 if [ ! -z "${LIBCURL_ARCHIVE}" ]; then
 	ARCHIVE="${SRCDIR}/${LIBCURL_ARCHIVE}"
 	if [ ! -f "${ARCHIVE}" ]; then
-		echo "Missing ${ARCHIVE}"
+		echo "Missing ${ARCHIVE}" >&2
 		exit 1
 	fi
 	# libcurl
@@ -160,7 +161,7 @@ fi
 if [ ! -z "${OPENSSL_ARCHIVE}" ]; then
 	ARCHIVE="${SRCDIR}/${OPENSSL_ARCHIVE}"
 	if [ ! -f "${ARCHIVE}" ]; then
-		echo "Missing ${ARCHIVE}"
+		echo "Missing ${ARCHIVE}" >&2
 		exit 1
 	fi
 	# OpenSSL
@@ -176,13 +177,39 @@ if [ ! -z "${OPENSSL_ARCHIVE}" ]; then
 fi
 
 
-if [ ! -z "${LIBISDS_GIT}" ]; then
+if [ ! -z "${LIBISDS_ARCHIVE}" -a ! -z "${LIBISDS_GIT}" ]; then
+	echo "Select libisds archive or git repository." >&2
+	exit 1
+elif [ ! -z "${LIBISDS_ARCHIVE}" ]; then
+	# libisds with OpenSSL back-end
+	ARCHIVE="${SRCDIR}/${LIBISDS_ARCHIVE}"
+	if [ ! -f "${ARCHIVE}" ]; then
+		echo "Missing ${ARCHIVE}" >&2
+		exit 1
+	fi
+	# libisds
+	rm -rf "${WORKDIR}"/libisds*
+	cd "${WORKDIR}"
+	tar -xJf "${ARCHIVE}"
+	cd "${WORKDIR}"/libisds*
+	# --disable-static
+	./configure --enable-debug --enable-openssl-backend --disable-fatalwarnings --prefix="${BUILTDIR}" --host="${X86_MINGV_HOST}" --with-xml-prefix="${BUILTDIR}" --with-libcurl="${BUILTDIR}" --with-libiconv-prefix="${BUILTDIR}" CPPFLAGS="-I${BUILTDIR}/include -I${BUILTDIR}/include/libxml2" LDFLAGS="-L${BUILTDIR}/lib"
+	make
+	cd src
+	#i686-pc-mingw32-gcc -shared -O2 -g -std=c99 -Wall -o .libs/libisds.dll libisds_la-cdecode.o libisds_la-cencode.o libisds_la-isds.o libisds_la-physxml.o libisds_la-utils.o libisds_la-validator.o libisds_la-crypto_openssl.o libisds_la-soap.o libisds_la-win32.o -L${BUILTDIR}/lib -lxml2 -liconv -lcurl -lexpat -lintl -lcrypto
+	#../libtool -v --tag=CC --mode=link i686-pc-mingw32-gcc  -g -O2 -g -std=c99 -Wall -version-info 8:0:3 -L${BUILTDIR}/lib -lxml2 -lz -L${BUILTDIR}/lib -liconv -L${BUILTDIR}/lib -lcurl -lwldap32 -lz -lws2_32 -lexpat -L${BUILTDIR}/lib -lintl -L${BUILTDIR}/lib -liconv -R${BUILTDIR}/lib -L${BUILTDIR}/lib -o libisds.la -rpath ${BUILTDIR}/lib libisds_la-cdecode.lo libisds_la-cencode.lo libisds_la-isds.lo libisds_la-physxml.lo libisds_la-utils.lo libisds_la-validator.lo libisds_la-crypto_openssl.lo  libisds_la-soap.lo libisds_la-win32.lo -L${BUILTDIR}/bin -leay32 -no-undefined
+	../libtool -v --tag=CC --mode=link ${X86_MINGW_CC}  -g -O2 -g -std=c99 -Wall -version-info 8:0:3 -L${BUILTDIR}/lib -lxml2 -lz -L${BUILTDIR}/lib -liconv -L${BUILTDIR}/lib -lcurl -lwldap32 -lz -lws2_32 -lexpat -L${BUILTDIR}/lib -lintl -L${BUILTDIR}/lib -liconv -R${BUILTDIR}/lib -L${BUILTDIR}/lib -o libisds.la -rpath ${BUILTDIR}/lib libisds_la-cdecode.lo libisds_la-cencode.lo libisds_la-isds.lo libisds_la-physxml.lo libisds_la-utils.lo libisds_la-validator.lo libisds_la-crypto_openssl.lo  libisds_la-soap.lo libisds_la-win32.lo -L${BUILTDIR}/bin -leay32 -no-undefined
+	cd ..
+	make install
+elif [ ! -z "${LIBISDS_GIT}" ]; then
 	# libisds with OpenSSL back-end
 	rm -rf "${WORKDIR}"/libisds*
 	cd "${WORKDIR}"
 	git clone "${LIBISDS_GIT}" libisds-git
 	cd "${WORKDIR}"/libisds*
-	git checkout "${LIBISDS_BRANCH}"
+	if [ ! -z "${LIBISDS_BRANCH}" ]; then
+		git checkout "${LIBISDS_BRANCH}"
+	fi
 	cat configure.ac | sed -e 's/AC_FUNC_MALLOC//g' > nomalloc_configure.ac
 	mv nomalloc_configure.ac configure.ac
 	autoheader && libtoolize -c --install && aclocal -I m4 && automake --add-missing --copy && autoconf && echo configure build ok
@@ -193,7 +220,7 @@ if [ ! -z "${LIBISDS_GIT}" ]; then
 	#../libtool -v --tag=CC --mode=link i686-pc-mingw32-gcc  -g -O2 -g -std=c99 -Wall -version-info 8:0:3 -L${BUILTDIR}/lib -lxml2 -lz -L${BUILTDIR}/lib -liconv -L${BUILTDIR}/lib -lcurl -lwldap32 -lz -lws2_32 -lexpat -L${BUILTDIR}/lib -lintl -L${BUILTDIR}/lib -liconv -R${BUILTDIR}/lib -L${BUILTDIR}/lib -o libisds.la -rpath ${BUILTDIR}/lib libisds_la-cdecode.lo libisds_la-cencode.lo libisds_la-isds.lo libisds_la-physxml.lo libisds_la-utils.lo libisds_la-validator.lo libisds_la-crypto_openssl.lo  libisds_la-soap.lo libisds_la-win32.lo -L${BUILTDIR}/bin -leay32 -no-undefined
 	../libtool -v --tag=CC --mode=link ${X86_MINGW_CC}  -g -O2 -g -std=c99 -Wall -version-info 8:0:3 -L${BUILTDIR}/lib -lxml2 -lz -L${BUILTDIR}/lib -liconv -L${BUILTDIR}/lib -lcurl -lwldap32 -lz -lws2_32 -lexpat -L${BUILTDIR}/lib -lintl -L${BUILTDIR}/lib -liconv -R${BUILTDIR}/lib -L${BUILTDIR}/lib -o libisds.la -rpath ${BUILTDIR}/lib libisds_la-cdecode.lo libisds_la-cencode.lo libisds_la-isds.lo libisds_la-physxml.lo libisds_la-utils.lo libisds_la-validator.lo libisds_la-crypto_openssl.lo  libisds_la-soap.lo libisds_la-win32.lo -L${BUILTDIR}/bin -leay32 -no-undefined
 	cd ..
-	#make install
+	make install
 fi
 
 #../libtool --tag=CC --mode=link i686-pc-mingw32-gcc -g -O2 -g -std=c99 -Wall -version-info 8:0:3 -R${BUILTDIR}/lib -L${BUILTDIR}/lib -o libisds.la -rpath ${BUILTDIR}/lib libisds_la-cdecode.lo libisds_la-cencode.lo libisds_la-isds.lo libisds_la-physxml.lo libisds_la-utils.lo libisds_la-validator.lo libisds_la-crypto_openssl.lo  libisds_la-soap.lo libisds_la-win32.lo -L${BUILTDIR}/lib -lxml2 -liconv -lcurl -lexpat -lintl -lcrypto
