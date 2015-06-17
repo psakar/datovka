51b48c6604187b2faf17a15f7f7d26bbf54b5c6a
Karel Slany <karel.slany@nic.cz>
2015-06-17 16:35:54 +0200

    Updated library dependencies.

--- scripts/dependency_sources.sh
+++ scripts/dependency_sources.sh
@@ -7,8 +7,8 @@ _LIBICONV_ARCHIVE="libiconv-1.14.tar.gz"
 _LIBXML2_ARCHIVE="libxml2-2.9.2.tar.gz"
 _GETTEXT_ARCHIVE="gettext-0.19.4.tar.xz"
 
-_LIBCURL_ARCHIVE="curl-7.42.1.tar.gz"
-_OPENSSL_ARCHIVE="openssl-1.0.1m.tar.gz"
+_LIBCURL_ARCHIVE="curl-7.43.0.tar.gz"
+_OPENSSL_ARCHIVE="openssl-1.0.1o.tar.gz"
 
 _LIBISDS_ARCHIVE="libisds-0.10.tar.xz"
 _LIBISDS_GIT="https://gitlab.labs.nic.cz/kslany/libisds.git"
