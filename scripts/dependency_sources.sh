41c29da779a2eeb39bdd351f6534f1901cd48644
Karel Slany <karel.slany@nic.cz>
2015-10-01 15:55:35 +0200

    Updated libraries which the application depends on.

--- scripts/dependency_sources.sh
+++ scripts/dependency_sources.sh
@@ -5,11 +5,11 @@ _LIBTOOL_ARCHIVE="libtool-2.4.6.tar.xz"
 
 _LIBICONV_ARCHIVE="libiconv-1.14.tar.gz"
 _LIBXML2_ARCHIVE="libxml2-2.9.2.tar.gz"
-_GETTEXT_ARCHIVE="gettext-0.19.4.tar.xz"
+_GETTEXT_ARCHIVE="gettext-0.19.6.tar.xz"
 
-_LIBCURL_ARCHIVE="curl-7.43.0.tar.gz"
-_OPENSSL_ARCHIVE="openssl-1.0.1o.tar.gz"
+_LIBCURL_ARCHIVE="curl-7.44.0.tar.gz"
+_OPENSSL_ARCHIVE="openssl-1.0.2d.tar.gz"
 
-_LIBISDS_ARCHIVE="libisds-0.10.tar.xz"
+_LIBISDS_ARCHIVE="libisds-0.10.1.tar.xz"
 _LIBISDS_GIT="https://gitlab.labs.nic.cz/kslany/libisds.git"
 #_LIBISDS_BRANCH="feature-openssl" # Use master.
