612d98fcad98051ec746f5a94bf49664946f03ca
Karel Slany <karel.slany@nic.cz>
2015-10-08 10:42:12 +0200

    Updated libcurl to version 7.45.

--- scripts/dependency_sources.sh
+++ scripts/dependency_sources.sh
@@ -7,7 +7,7 @@ _LIBICONV_ARCHIVE="libiconv-1.14.tar.gz"
 _LIBXML2_ARCHIVE="libxml2-2.9.2.tar.gz"
 _GETTEXT_ARCHIVE="gettext-0.19.6.tar.xz"
 
-_LIBCURL_ARCHIVE="curl-7.44.0.tar.gz"
+_LIBCURL_ARCHIVE="curl-7.45.0.tar.gz"
 _OPENSSL_ARCHIVE="openssl-1.0.2d.tar.gz"
 
 _LIBISDS_ARCHIVE="libisds-0.10.1.tar.xz"
