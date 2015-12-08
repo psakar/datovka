f5c0ebccb508bf6b5483554c450d6440f924facc
Karel Slany <karel.slany@nic.cz>
2015-12-08 12:06:37 +0100

    Updated library versions.

--- scripts/dependency_sources.sh
+++ scripts/dependency_sources.sh
@@ -4,11 +4,11 @@ _EXPAT_ARCHIVE="expat-2.1.0.tar.gz"
 _LIBTOOL_ARCHIVE="libtool-2.4.6.tar.xz"
 
 _LIBICONV_ARCHIVE="libiconv-1.14.tar.gz"
-_LIBXML2_ARCHIVE="libxml2-2.9.2.tar.gz"
+_LIBXML2_ARCHIVE="libxml2-2.9.3.tar.gz"
 _GETTEXT_ARCHIVE="gettext-0.19.6.tar.xz"
 
-_LIBCURL_ARCHIVE="curl-7.45.0.tar.gz"
-_OPENSSL_ARCHIVE="openssl-1.0.2d.tar.gz"
+_LIBCURL_ARCHIVE="curl-7.46.0.tar.gz"
+_OPENSSL_ARCHIVE="openssl-1.0.2e.tar.gz"
 
 _LIBISDS_ARCHIVE="libisds-0.10.1.tar.xz"
 _LIBISDS_GIT="https://gitlab.labs.nic.cz/kslany/libisds.git"
