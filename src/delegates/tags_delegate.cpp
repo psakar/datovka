aa374e05813f06db8e89b4e318ef9e136adcb0bb
Karel Slany <karel.slany@nic.cz>
2016-03-22 17:03:51 +0100

    Delegates draw their data directly from the model.

--- src/delegates/tags_delegate.cpp
+++ src/delegates/tags_delegate.cpp
@@ -33,64 +33,22 @@ TagsDelegate::TagsDelegate(QWidget *parent)
 void TagsDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option,
     const QModelIndex &index) const
 {
-	if (index.column() == 7) {
-//		logInfoNL("%s", "AAA001");
-		static TagItemList tagList;
-		if (tagList.isEmpty()) {
-			tagList.append(TagItem(0, "one", "ff0000"));
-			tagList.append(TagItem(0, "two", "00ff00"));
-			tagList.append(TagItem(0, "three", "0000ff"));
-			tagList.append(TagItem(0, "four", "ef4444"));
-			tagList.append(TagItem(0, "five", "faa31b"));
-			tagList.append(TagItem(0, "six", "fff000"));
-			tagList.append(TagItem(0, "seven", "82c341"));
-			tagList.append(TagItem(0, "eight", "009f75"));
-			tagList.append(TagItem(0, "nine", "88c6ed"));
-			tagList.append(TagItem(0, "ten", "394ba0"));
-			tagList.append(TagItem(0, "eleven", "d54799"));
-		}
-
+	if (index.data().canConvert<TagItemList>()) {
+		TagItemList tagList = qvariant_cast<TagItemList>(index.data());
 		tagList.paint(painter, option.rect, option.font,
 		    option.palette);
-		return;
-	}
-
-//	if (index.data().canConvert<TagItemList>()) {
-//		TagItemList tagList = qvariant_cast<TagItemList>(index.data());
-//		/* TODO */
-//		tagList.paint(painter, option.rect, option.palette);
-//	} else {
+	} else {
 		QStyledItemDelegate::paint(painter, option, index);
-//	}
+	}
 }
 
 QSize TagsDelegate::TagsDelegate::sizeHint(const QStyleOptionViewItem &option,
     const QModelIndex &index) const
 {
-	if (index.column() == 7) {
-//		logInfoNL("%s", "AAA000");
-		static TagItemList tagList;
-		if (tagList.isEmpty()) {
-			tagList.append(TagItem(0, "one", "ff0000"));
-			tagList.append(TagItem(0, "two", "00ff00"));
-			tagList.append(TagItem(0, "three", "0000ff"));
-			tagList.append(TagItem(0, "four", "ef4444"));
-			tagList.append(TagItem(0, "five", "faa31b"));
-			tagList.append(TagItem(0, "six", "fff000"));
-			tagList.append(TagItem(0, "seven", "82c341"));
-			tagList.append(TagItem(0, "eight", "009f75"));
-			tagList.append(TagItem(0, "nine", "88c6ed"));
-			tagList.append(TagItem(0, "ten", "394ba0"));
-			tagList.append(TagItem(0, "eleven", "d54799"));
-		}
-
+	if (index.data().canConvert<TagItemList>()) {
+		TagItemList tagList = qvariant_cast<TagItemList>(index.data());
 		return tagList.sizeHint(option.rect, option.font);
-	}
-
-//	if (index.data().canConvert<TagItemList>()) {
-//		TagItemList tagList = qvariant_cast<TagItemList>(index.data());
-//		return tagList.sizeHint();
-//	} else {
+	} else {
 		return QStyledItemDelegate::sizeHint(option, index);
-//	}
+	}
 }
