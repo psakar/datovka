87ff4b3c7937422d4ff14c613d9c9a2cc4aa46eb
Karel Slany <karel.slany@nic.cz>
2016-10-05 19:54:03 +0200

    Experimenting with coverity models.

---
+++ coverity_model.cpp
@@ -0,0 +1,19 @@
+/* Coverity Scan model
+ *
+ * This is a modelling file for Coverity Scan. Modelling helps to avoid false
+ * positives.
+ *
+ * - A model file can't import any header files.
+ * - Therefore only some built-in primitives like int, char and void are
+ *   available but not wchar_t, NULL etc.
+ * - Modelling doesn't need full structs and typedefs. Rudimentary structs
+ *   and similar types are sufficient.
+ * - An uninitialized local pointer is not an error. It signifies that the
+ *   variable could be either NULL or have some data.
+ *
+ * Coverity Scan doesn't pick up modifications automatically. The model file
+ * must be uploaded by an admin in the analysis settings of
+ * http://scan.coverity.com/projects/3079
+ */
+
+/* Nothing to model in C++. */
