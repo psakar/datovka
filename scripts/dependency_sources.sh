702c032af426c4441944a908e4a7aca2e9497f94
Karel Slany <karel.slany@nic.cz>
2016-10-17 15:23:08 +0200

    Bundling latest libisds (0.10.6).

--- scripts/dependency_sources.sh
+++ scripts/dependency_sources.sh
@@ -12,7 +12,7 @@ _GETTEXT_ARCHIVE="gettext-0.19.8.1.tar.xz"
 _LIBCURL_ARCHIVE="curl-7.50.3.tar.bz2"
 _OPENSSL_ARCHIVE="openssl-1.0.2j.tar.gz"
 
-_LIBISDS_ARCHIVE="libisds-0.10.5.tar.xz"
+_LIBISDS_ARCHIVE="libisds-0.10.6.tar.xz"
 _LIBISDS_ARCHIVE_PATCHES=" \
 	"
 _LIBISDS_GIT="https://gitlab.labs.nic.cz/kslany/libisds.git"
