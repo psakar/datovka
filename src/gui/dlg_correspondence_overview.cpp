8021fe264ef521dc0ffbc97e6bf5949afc8451ca
Karel Slany <karel.slany@nic.cz>
2017-06-07 14:19:29 +0200

    Adjusted HTML correspondence overview page to pass HTML validation test.

--- src/gui/dlg_correspondence_overview.cpp
+++ src/gui/dlg_correspondence_overview.cpp
@@ -382,7 +382,7 @@ bool DlgCorrespondenceOverview::writeHtmlOverview(const QString &userName,
 	f.setCodec("UTF-8");
 	/* Generate HTML header. */
 	f << QStringLiteral("<!DOCTYPE html\n"
-	    "   PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\""
+	    "   PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" "
 	    "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n"
 	    "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n"
 	    "<head>\n"
