8371c8223b6e183aa34f6ad5a2869731c0f65437
Karel Slany <karel.slany@nic.cz>
2017-06-10 09:50:28 +0200

    Updated libraries.

--- scripts/dependency_sources.sh
+++ scripts/dependency_sources.sh
@@ -9,8 +9,8 @@ _LIBICONV_ARCHIVE="libiconv-1.15.tar.gz"
 _LIBXML2_ARCHIVE="libxml2-2.9.4.tar.gz"
 _GETTEXT_ARCHIVE="gettext-0.19.8.1.tar.xz"
 
-_LIBCURL_ARCHIVE="curl-7.53.1.tar.bz2"
-_OPENSSL_ARCHIVE="openssl-1.0.2k.tar.gz"
+_LIBCURL_ARCHIVE="curl-7.54.0.tar.bz2"
+_OPENSSL_ARCHIVE="openssl-1.0.2l.tar.gz"
 
 _LIBISDS_ARCHIVE="libisds-0.10.6.tar.xz"
 _LIBISDS_ARCHIVE_PATCHES=" \
