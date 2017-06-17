af80e7f55272e881bac8c03e3db4755778605218
Karel Slany <karel.slany@nic.cz>
2017-06-17 17:22:42 +0200

    Storing stored_files response into database.

--- src/worker/task_document_service_download_stored_messages.cpp
+++ src/worker/task_document_service_download_stored_messages.cpp
@@ -21,10 +21,12 @@
  * the two.
  */
 
+#include <cinttypes>
 #include <QByteArray>
 #include <QSet>
 #include <QThread>
 
+#include "src/document_service/json/entry_error.h"
 #include "src/document_service/json/stored_files.h"
 #include "src/log/log.h"
 #include "src/worker/message_emitter.h"
@@ -92,8 +94,133 @@ QList<qint64> obtainDmIds(MessageDbSet *dbSet,
 	return dmIdSet.toList();
 }
 
+/*!
+ * @brief Checks whether response contains all requested entries.
+ *
+ * @param[in] sfRes Stored files response structure
+ * @param[in] sentDmIds Set identifiers.
+ * @return True if response contains requested entries.
+ */
 static
-int callStoredFiles(DocumentServiceConnection &dsc, const QList<qint64> &dmIds)
+bool receivedRequestedContent(const StoredFilesResp &sfRes,
+    const QList<qint64> &sentDmIds)
+{
+	QSet<qint64> sentDmIdSet(sentDmIds.toSet());
+
+	foreach (const DmEntry &entry, sfRes.dms()) {
+		if (!sentDmIdSet.remove(entry.dmId())) {
+			logErrorNL(
+			    "Obtained response for message '%" PRId64 "'that has not been requested.",
+			    entry.dmId());
+			return false;
+		}
+	}
+
+	if (!sentDmIdSet.isEmpty()) {
+		logErrorNL("%s", "Did not obtain all requested message entries.");
+		return false;
+	}
+
+	return true;
+}
+
+/*!
+ * @brief Store response into database.
+ *
+ * @param[in] sfRes Stored files response structure.
+ * @return True on success, false on error.
+ */
+static
+bool storeStoredFilesResponseContent(const StoredFilesResp &sfRes)
+{
+	/* Process only messages. */
+
+	if (Q_NULLPTR == globDocumentServiceDbPtr) {
+		return false;
+	}
+
+	if (!globDocumentServiceDbPtr->beginTransaction()) {
+		return false;
+	}
+
+	foreach (const DmEntry &entry, sfRes.dms()) {
+		if (entry.locations().isEmpty()) {
+			/* Not held within the document service. */
+			continue;
+		}
+
+		if (!globDocumentServiceDbPtr->updateStoredMsg(entry.dmId(),
+		        entry.locations())) {
+			logErrorNL(
+			    "Could not update information about message '%" PRId64 "'.",
+			    entry.dmId());
+			goto fail;
+		}
+	}
+
+	if (!globDocumentServiceDbPtr->commitTransaction()) {
+		goto fail;
+	}
+	return true;
+
+fail:
+	globDocumentServiceDbPtr->rollbackTransaction();
+	return false;
+}
+
+/*!
+ * @brief Process response.
+ *
+ * @param[in]  sfRes Stored files response structure.
+ * @param[in]  sentDmIds Set identifiers.
+ * @param[out] limit Obtained limit.
+ * @return Number of processed responses, negative value on error.
+ */
+static
+int processStoredFilesResponse(const StoredFilesResp &sfRes,
+    const QList<qint64> &sentDmIds, int &limit)
+{
+	/* Limit should always be received. */
+	if (sfRes.limit() <= 0) {
+		Q_ASSERT(0);
+		return -1;
+	}
+	limit = sfRes.limit();
+
+	switch (sfRes.error().code()) {
+	case ErrorEntry::ERR_NO_ERROR:
+		break;
+	case ErrorEntry::ERR_LIMIT_EXCEEDED:
+		return 0; /* Nothing was processed. Should ask again. */
+		break;
+	default:
+		logErrorNL("Received error '%s'.", sfRes.error().trVerbose());
+		return -1;
+		break;
+	}
+
+	if (!receivedRequestedContent(sfRes, sentDmIds)) {
+		return -1;
+	}
+
+	if (!storeStoredFilesResponseContent(sfRes)) {
+		return -1;
+	}
+
+	return sfRes.dms().size();
+}
+
+/*!
+ * @brief Call document service and process response.
+ *
+ * @param[in]  dsc Document service connection.
+ * @param[in]  dmIds Message identifiers.
+ * @param[out] limit Obtained limit.
+ * @return Number of processed responses, negative value on error.
+ */
+static
+int callStoredFiles(DocumentServiceConnection &dsc, const QList<qint64> &dmIds,
+    int &limit)
 {
 	if (dmIds.isEmpty()) {
 		Q_ASSERT(0);
@@ -103,6 +230,7 @@ int callStoredFiles(DocumentServiceConnection &dsc, const QList<qint64> &dmIds)
 	StoredFilesReq sfReq(dmIds, QList<qint64>());
 	if (!sfReq.isValid()) {
 		logErrorNL("%s", "Could not create stored_files request.");
+		return -1;
 	}
 
 	QByteArray response;
@@ -119,7 +247,7 @@ int callStoredFiles(DocumentServiceConnection &dsc, const QList<qint64> &dmIds)
 				return -1;
 			}
 
-			return 0; /* TODO */
+			return processStoredFilesResponse(sfRes, dmIds, limit);
 		} else {
 			logErrorNL("%s",
 			    "Communication error. Received empty response to stored_files service.");
@@ -144,16 +272,19 @@ enum TaskDocumentServiceDownloadStoredMessages::Result updateMessages(
 	dsc.setConnection(urlStr, tokenStr);
 
 	int pos = 0; /* Position. */
-	int incr = 1; /* Start with smallest query to obtain maximal size. */
+	int currentLimit = 1; /* Start with smallest query to obtain maximal size. */
+	int nextLimit = 0;
 
 	/* While list is not processed. */
 	while ((pos >= 0) && (pos < dmIds.size())) {
-		QList<qint64> queryList(dmIds.mid(pos, incr));
+		QList<qint64> queryList(dmIds.mid(pos, currentLimit));
 
-		int ret = callStoredFiles(dsc, queryList);
-		break; /* TODO */
-
-		pos += incr;
+		int ret = callStoredFiles(dsc, queryList, nextLimit);
+		if (ret < 0) {
+			return TaskDocumentServiceDownloadStoredMessages::DS_DSM_ERR;
+		}
+		pos += ret;
+		currentLimit = nextLimit; /* Update limit. */
 	}
 
 	return TaskDocumentServiceDownloadStoredMessages::DS_DSM_ERR;
