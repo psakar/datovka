368bbc4b1f7f634af81f07385c7bcc0e03f5b238
Karel Slany <karel.slany@nic.cz>
2017-07-25 13:36:44 +0200

    Copying Qt libraries to test application directory.

--- tests/records_management_app/build_rm_test_app.bat
+++ tests/records_management_app/build_rm_test_app.bat
@@ -2,15 +2,19 @@
 REM Ensure that path to Qt (C:\Qt\5.9.1\mingw53_32\bin) is set.
 REM Ensure that path to MinGW tools bundled with Qt (C:\Qt\Tools\mingw530_32\bin) is set.
 
+SET APP_DIR="rm_test_app"
+
 SET QMAKE="qmake.exe"
 SET MINGW_PATH="C:\Qt\Tools\mingw530_32"
 SET MAKE="mingw32-make.exe"
+SET DEPLOYQT="windeployqt.exe"
 
-"%QMAKE%" CONFIG+=debug rm_test_app.pro
+"%QMAKE%" CONFIG+=release rm_test_app.pro
 "%MAKE%" -j 4
 
-mkdir rm_test_app
-copy debug\rm_test_app.exe rm_test_app
-copy "%MINGW_PATH%\opt\bin\libeay32.dll" rm_test_app
-copy "%MINGW_PATH%\opt\bin\ssleay32.dll" rm_test_app
-copy rm_test_app.bat rm_test_app
+mkdir "%APP_DIR%"
+copy debug\rm_test_app.exe "%APP_DIR%"
+copy "%MINGW_PATH%\opt\bin\libeay32.dll" "%APP_DIR%"
+copy "%MINGW_PATH%\opt\bin\ssleay32.dll" "%APP_DIR%"
+copy rm_test_app.bat "%APP_DIR%"
+"%DEPLOYQT%" --dir "%APP_DIR%" --release "%APP_DIR%\rm_test_app.exe"
