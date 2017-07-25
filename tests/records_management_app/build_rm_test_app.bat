5a6fe21d4163882be20c55d9022f26d873bac380
Karel Slany <karel.slany@nic.cz>
2017-07-25 13:41:54 +0200

    Fixed error in test app building script.

--- tests/records_management_app/build_rm_test_app.bat
+++ tests/records_management_app/build_rm_test_app.bat
@@ -9,12 +9,14 @@ SET MINGW_PATH="C:\Qt\Tools\mingw530_32"
 SET MAKE="mingw32-make.exe"
 SET DEPLOYQT="windeployqt.exe"
 
-"%QMAKE%" CONFIG+=release rm_test_app.pro
+SET CONF="release"
+
+"%QMAKE%" CONFIG+=%CONF% rm_test_app.pro
 "%MAKE%" -j 4
 
 mkdir "%APP_DIR%"
-copy debug\rm_test_app.exe "%APP_DIR%"
+copy "%CONF%\rm_test_app.exe" "%APP_DIR%"
 copy "%MINGW_PATH%\opt\bin\libeay32.dll" "%APP_DIR%"
 copy "%MINGW_PATH%\opt\bin\ssleay32.dll" "%APP_DIR%"
 copy rm_test_app.bat "%APP_DIR%"
-"%DEPLOYQT%" --dir "%APP_DIR%" --release "%APP_DIR%\rm_test_app.exe"
+"%DEPLOYQT%" --dir "%APP_DIR%" --%CONF% "%APP_DIR%\rm_test_app.exe"
