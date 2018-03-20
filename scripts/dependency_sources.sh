840ced5fb271c5267117c6969c447b8381fdf1b2
Karel Slany <karel.slany@nic.cz>
2018-03-20 14:00:29 +0100

    Checking GPG signatures when downloading library sources.

--- scripts/dependency_sources.sh
+++ scripts/dependency_sources.sh
@@ -2,35 +2,53 @@
 
 # Latest libraries.
 _ZLIB_ARCHIVE="zlib-1.2.11.tar.xz"
-_ZLIB_SHA256="4ff941449631ace0d4d203e3483be9dbc9da454084111f97ea0a2114e19bf066"
 _ZLIB_URL_PREFIX="http://zlib.net/"
+_ZLIB_SHA256="4ff941449631ace0d4d203e3483be9dbc9da454084111f97ea0a2114e19bf066"
+_ZLIB_SIG_SUFF=".asc"
+_ZLIB_KEY_FP="5ED46A6721D365587791E2AA783FCD8E58BCAFBA" # "Mark Adler <madler@alumni.caltech.edu>"
 _EXPAT_ARCHIVE="expat-2.2.5.tar.bz2"
-_EXPAT_SHA256="d9dc32efba7e74f788fcc4f212a43216fc37cf5f23f4c2339664d473353aedf6"
 _EXPAT_URL_PREFIX="https://github.com/libexpat/libexpat/releases/download/R_2_2_5/"
+_EXPAT_SHA256="d9dc32efba7e74f788fcc4f212a43216fc37cf5f23f4c2339664d473353aedf6"
+_EXPAT_SIG_SUFF=""
+_EXPAT_KEY_FP="" #
 _LIBTOOL_ARCHIVE="libtool-2.4.6.tar.xz"
-_LIBTOOL_SHA256="7c87a8c2c8c0fc9cd5019e402bed4292462d00a718a7cd5f11218153bf28b26f"
 _LIBTOOL_URL_PREFIX="http://ftpmirror.gnu.org/libtool/"
+_LIBTOOL_SHA256="7c87a8c2c8c0fc9cd5019e402bed4292462d00a718a7cd5f11218153bf28b26f"
+_LIBTOOL_SIG_SUFF=".sig"
+_LIBTOOL_KEY_FP="CFE2BE707B538E8B26757D84151308092983D606" # "Gary Vaughan (Free Software Developer) <gary@vaughan.pe>"
 
 _LIBICONV_ARCHIVE="libiconv-1.15.tar.gz"
-_LIBICONV_SHA256="ccf536620a45458d26ba83887a983b96827001e92a13847b45e4925cc8913178"
 _LIBICONV_URL_PREFIX="https://ftp.gnu.org/pub/gnu/libiconv/"
+_LIBICONV_SHA256="ccf536620a45458d26ba83887a983b96827001e92a13847b45e4925cc8913178"
+_LIBICONV_SIG_SUFF=".sig"
+_LIBICONV_KEY_FP="68D94D8AAEEAD48AE7DC5B904F494A942E4616C2" # "Bruno Haible (Open Source Development) <bruno@clisp.org>"
 _LIBXML2_ARCHIVE="libxml2-2.9.8.tar.gz"
-_LIBXML2_SHA256="0b74e51595654f958148759cfef0993114ddccccbb6f31aee018f3558e8e2732"
 _LIBXML2_URL_PREFIX="ftp://xmlsoft.org/libxml2/"
+_LIBXML2_SHA256="0b74e51595654f958148759cfef0993114ddccccbb6f31aee018f3558e8e2732"
+_LIBXML2_SIG_SUFF=".asc"
+_LIBXML2_KEY_FP="DB46681BB91ADCEA170FA2D415588B26596BEA5D" # "Daniel Veillard (Red Hat work email) <veillard@redhat.com>"
 _GETTEXT_ARCHIVE="gettext-0.19.8.1.tar.xz"
-_GETTEXT_SHA256="105556dbc5c3fbbc2aa0edb46d22d055748b6f5c7cd7a8d99f8e7eb84e938be4"
 _GETTEXT_URL_PREFIX="http://ftp.gnu.org/pub/gnu/gettext/"
+_GETTEXT_SHA256="105556dbc5c3fbbc2aa0edb46d22d055748b6f5c7cd7a8d99f8e7eb84e938be4"
+_GETTEXT_SIG_SUFF=".sig"
+_GETTEXT_KEY_FP="462225C3B46F34879FC8496CD605848ED7E69871" # "Daiki Ueno <ueno@unixuser.org>"
 
 _LIBCURL_ARCHIVE="curl-7.59.0.tar.xz"
-_LIBCURL_SHA256="e44eaabdf916407585bf5c7939ff1161e6242b6b015d3f2f5b758b2a330461fc"
 _LIBCURL_URL_PREFIX="https://curl.haxx.se/download/"
+_LIBCURL_SHA256="e44eaabdf916407585bf5c7939ff1161e6242b6b015d3f2f5b758b2a330461fc"
+_LIBCURL_SIG_SUFF=".asc"
+_LIBCURL_KEY_FP="27EDEAF22F3ABCEB50DB9A125CC908FDB71E12C2" # "Daniel Stenberg <daniel@haxx.se>"
 _OPENSSL_ARCHIVE="openssl-1.0.2n.tar.gz"
-_OPENSSL_SHA256="370babb75f278c39e0c50e8c4e7493bc0f18db6867478341a832a982fd15a8fe"
 _OPENSSL_URL_PREFIX="https://www.openssl.org/source/"
+_OPENSSL_SHA256="370babb75f278c39e0c50e8c4e7493bc0f18db6867478341a832a982fd15a8fe"
+_OPENSSL_SIG_SUFF=".asc"
+_OPENSSL_KEY_FP="8657ABB260F056B1E5190839D9C4D26D0E604491" # "Matt Caswell <matt@openssl.org>"
 
 _LIBISDS_ARCHIVE="libisds-0.10.7.tar.xz"
-_LIBISDS_SHA256="8a738d3bf0f4dd150fe633607cc9a4d29cd62b61e1d2acf38cedf265b5f08589"
 _LIBISDS_URL_PREFIX="http://xpisar.wz.cz/libisds/dist/"
+_LIBISDS_SHA256="8a738d3bf0f4dd150fe633607cc9a4d29cd62b61e1d2acf38cedf265b5f08589"
+_LIBISDS_SIG_SUFF=".asc"
+_LIBISDS_KEY_FP="4B528393E6A3B0DFB2EF3A6412C9C5C767C6FAA2" # "Petr Pisar <petr.pisar@atlas.cz>"
 _LIBISDS_ARCHIVE_PATCHES=" \
 	"
 _LIBISDS_GIT="https://gitlab.labs.nic.cz/kslany/libisds.git"
@@ -80,8 +98,52 @@ check_sha256 () {
 	return 0
 }
 
-# Download source.
-download_source () {
+# Check signature.
+check_sig () {
+	local FILE_NAME="$1"
+	local SIG_FILE_NAME="$2"
+	local KEY_FP="$3"
+
+	local CMD_GPG=gpg
+
+	if [ "x${FILE_NAME}" = "x" ]; then
+		echo "No file name supplied." >&2
+		return 1
+	fi
+	if [ "x${SIG_FILE_NAME}" = "x" ]; then
+		echo "No signature file name supplied." >&2
+		return 1
+	fi
+	if [ "x${KEY_FP}" = "x" ]; then
+		echo "Missing key fingerprint to signature of '${ARCHIVE_FILE}'." >&2
+		return 1
+	fi
+
+	if [ -z $(command -v "${CMD_GPG}") ]; then
+		echo "Install '${CMD_GPG}' to be able to compute file checksum." >&2
+		return 1
+	fi
+
+	local GPG_DIR="./.gnupg_work"
+	rm -rf "${GPG_DIR}"
+	mkdir -p "${GPG_DIR}"
+
+	local GPG_PARAMS="--homedir ${GPG_DIR} --no-default-keyring --keyring keys.gpg --no-permission-warning"
+	"${CMD_GPG}" ${GPG_PARAMS} --keyserver sks.labs.nic.cz --recv "${KEY_FP}"
+	"${CMD_GPG}" ${GPG_PARAMS} --verify "${SIG_FILE_NAME}" "${FILE_NAME}"
+	local GPG_RET="$?"
+	rm -rf "${GPG_DIR}"
+	if [ "${GPG_RET}" != "0" ]; then
+		echo "'${FILE_NAME}' signature check failed." >&2
+		return 1
+	fi
+
+	echo "'${FILE_NAME}' signature OK."
+	return 0
+}
+
+# Download file.
+download_file () {
 	local ARCHIVE_FILE="$1"
 	local URL_PREFIX="$2"
 
@@ -109,50 +171,102 @@ download_source () {
 ensure_source_presence () {
 	local SRC_DIR="$1"
 	local ARCHIVE_FILE="$2"
-	local SHA256_HASH="$3"
-	local URL_PREFIX="$4"
+	local URL_PREFIX="$3"
+	local SHA256_HASH="$4"
+	local SIG_SUF="$5"
+	local KEY_FP="$6"
 
 	local ORIG_DIR=$(pwd)
+	local ARCHIVE_SIG_FILE=""
 
 	if [ "x${SRC_DIR}" = "x" -o ! -d "${SRC_DIR}" ]; then
 		echo "'${SRC_DIR}' is not a directory." >&2
 		return 1
 	fi
 	if [ "x${ARCHIVE_FILE}" = "x" -o -e "${SRC_DIR}/${ARCHIVE_FILE}" -a ! -f "${SRC_DIR}/${ARCHIVE_FILE}" ]; then
-		echo "Missing archive file argument or '${SRC_DIR}/${ARCHIVE_FILE}' is not a file." >&2
-		return 1
-	fi
-	if [ "x${SHA256_HASH}" = "x" ]; then
-		echo "No sha256 checksum for '${FILE_NAME}' supplied." >&2
+		echo "Missing archive file '${ARCHIVE_FILE}' argument or '${SRC_DIR}/${ARCHIVE_FILE}' is not a file." >&2
 		return 1
 	fi
 	if [ "x${URL_PREFIX}" = "x" ]; then
 		echo "Missing URL prefix for archive '${ARCHIVE_FILE}'." >&2
 		return 1
 	fi
-
-	cd "${SRC_DIR}"
-
-	if [ -e "${ARCHIVE_FILE}" ]; then
-		check_sha256 "${ARCHIVE_FILE}" "${SHA256_HASH}" && return 0
-		# Checksum mismatch.
-		echo "Trying to download '${ARCHIVE_FILE}'." >&2
-		mv "${ARCHIVE_FILE}" "bad_${ARCHIVE_FILE}"
+	if [ "x${SHA256_HASH}" = "x" ]; then
+		echo "No sha256 checksum for '${FILE_NAME}' supplied." >&2
+		return 1
 	fi
-
-	if [ ! -e "${ARCHIVE_FILE}" ]; then
-		if ! download_source "${ARCHIVE_FILE}" "${URL_PREFIX}"; then
-			echo "Cannot download '${ARCHIVE_FILE}'." >&2
+	if [ "x${SIG_SUF}" != "x" ]; then
+		ARCHIVE_SIG_FILE="${ARCHIVE_FILE}${SIG_SUF}"
+		if [ -e "${SRC_DIR}/${ARCHIVE_SIG_FILE}" -a ! -f "${SRC_DIR}/${ARCHIVE_SIG_FILE}" ]; then
+			echo "'${SRC_DIR}/${ARCHIVE_SIG_FILE}' is not a file." >&2
+			return 1
+		fi
+		if [ "x${KEY_FP}" = "x" ]; then
+			echo "Missing key fingerprint to signature of '${ARCHIVE_FILE}'." >&2
 			return 1
 		fi
 	fi
 
+	cd "${SRC_DIR}"
+
+	# Download signature file.
+	if [ "x${ARCHIVE_SIG_FILE}" != "x" -a ! -e "${ARCHIVE_SIG_FILE}" ]; then
+		if ! download_file "${ARCHIVE_SIG_FILE}" "${URL_PREFIX}"; then
+			echo "Cannot download '${ARCHIVE_SIG_FILE}'." >&2
+			return 1
+		fi
+		if [ ! -f "${ARCHIVE_SIG_FILE}" ]; then
+			echo "'${ARCHIVE_SIG_FILE}' is missing." >&2
+			return 1
+		fi
+	fi
+
+	if [ -e "${ARCHIVE_FILE}" ]; then
+		if check_sha256 "${ARCHIVE_FILE}" "${SHA256_HASH}"; then
+			if [ "x${ARCHIVE_SIG_FILE}" != "x" ]; then
+				check_sig "${ARCHIVE_FILE}" "${ARCHIVE_SIG_FILE}" "${KEY_FP}" && return 0
+			else
+				return 0
+			fi
+		fi
+
+		# Checksum mismatch.
+		echo "Trying to download '${ARCHIVE_FILE}'." >&2
+		mv "${ARCHIVE_FILE}" "bad_${ARCHIVE_FILE}"
+		if [ "x${ARCHIVE_SIG_FILE}" != "x" ]; then
+			echo "Trying to download '${ARCHIVE_SIG_FILE}'." >&2
+			mv "${ARCHIVE_SIG_FILE}" "bad_${ARCHIVE_SIG_FILE}"
+		fi
+	fi
+
+	# Download source file.
 	if [ ! -e "${ARCHIVE_FILE}" ]; then
+		if ! download_file "${ARCHIVE_FILE}" "${URL_PREFIX}"; then
+			echo "Cannot download '${ARCHIVE_FILE}'." >&2
+			return 1
+		fi
+	fi
+	if [ ! -f "${ARCHIVE_FILE}" ]; then
 		echo "'${ARCHIVE_FILE}' is missing." >&2
 		return 1
 	fi
 
+	# Download signature file.
+	if [ "x${ARCHIVE_SIG_FILE}" != "x" -a ! -e "${ARCHIVE_SIG_FILE}" ]; then
+		if ! download_file "${ARCHIVE_SIG_FILE}" "${URL_PREFIX}"; then
+			echo "Cannot download '${ARCHIVE_SIG_FILE}'." >&2
+			return 1
+		fi
+		if [ ! -f "${ARCHIVE_SIG_FILE}" ]; then
+			echo "'${ARCHIVE_SIG_FILE}' is missing." >&2
+			return 1
+		fi
+	fi
+
 	check_sha256 "${ARCHIVE_FILE}" "${SHA256_HASH}" || return 1
+	if [ "x${ARCHIVE_SIG_FILE}" != "x" ]; then
+		check_sig "${ARCHIVE_FILE}" "${ARCHIVE_SIG_FILE}" "${KEY_FP}" || return 1
+	fi
 
 	cd "${ORIG_DIR}"
 	return 0
@@ -167,18 +281,27 @@ download_all_sources () {
 		return 1
 	fi
 
-	ensure_source_presence "${SRC_DIR}" "${_ZLIB_ARCHIVE}" "${_ZLIB_SHA256}" "${_ZLIB_URL_PREFIX}" || return 1
-	ensure_source_presence "${SRC_DIR}" "${_EXPAT_ARCHIVE}" "${_EXPAT_SHA256}" "${_EXPAT_URL_PREFIX}" || return 1
-	ensure_source_presence "${SRC_DIR}" "${_LIBTOOL_ARCHIVE}" "${_LIBTOOL_SHA256}" "${_LIBTOOL_URL_PREFIX}" || return 1
+	ensure_source_presence "${SRC_DIR}" "${_ZLIB_ARCHIVE}" "${_ZLIB_URL_PREFIX}" \
+	    "${_ZLIB_SHA256}" "${_ZLIB_SIG_SUFF}" "${_ZLIB_KEY_FP}" || return 1
+	ensure_source_presence "${SRC_DIR}" "${_EXPAT_ARCHIVE}" "${_EXPAT_URL_PREFIX}" \
+	    "${_EXPAT_SHA256}" "${_EXPAT_SIG_SUFF}" "${_EXPAT_KEY_FP}" || return 1
+	ensure_source_presence "${SRC_DIR}" "${_LIBTOOL_ARCHIVE}" "${_LIBTOOL_URL_PREFIX}" \
+	    "${_LIBTOOL_SHA256}" "${_LIBTOOL_SIG_SUFF}" "${_LIBTOOL_KEY_FP}" || return 1
 
-	ensure_source_presence "${SRC_DIR}" "${_LIBICONV_ARCHIVE}" "${_LIBICONV_SHA256}" "${_LIBICONV_URL_PREFIX}" || return 1
-	ensure_source_presence "${SRC_DIR}" "${_LIBXML2_ARCHIVE}" "${_LIBXML2_SHA256}" "${_LIBXML2_URL_PREFIX}" || return 1
-	ensure_source_presence "${SRC_DIR}" "${_GETTEXT_ARCHIVE}" "${_GETTEXT_SHA256}" "${_GETTEXT_URL_PREFIX}" || return 1
+	ensure_source_presence "${SRC_DIR}" "${_LIBICONV_ARCHIVE}" "${_LIBICONV_URL_PREFIX}" \
+	    "${_LIBICONV_SHA256}" "${_LIBICONV_SIG_SUFF}" "${_LIBICONV_KEY_FP}" || return 1
+	ensure_source_presence "${SRC_DIR}" "${_LIBXML2_ARCHIVE}" "${_LIBXML2_URL_PREFIX}" \
+	    "${_LIBXML2_SHA256}" "${_LIBXML2_SIG_SUFF}" "${_LIBXML2_KEY_FP}" || return 1
+	ensure_source_presence "${SRC_DIR}" "${_GETTEXT_ARCHIVE}" "${_GETTEXT_URL_PREFIX}" \
+	    "${_GETTEXT_SHA256}" "${_GETTEXT_SIG_SUFF}" "${_GETTEXT_KEY_FP}" || return 1
 
-	ensure_source_presence "${SRC_DIR}" "${_LIBCURL_ARCHIVE}" "${_LIBCURL_SHA256}" "${_LIBCURL_URL_PREFIX}" || return 1
-	ensure_source_presence "${SRC_DIR}" "${_OPENSSL_ARCHIVE}" "${_OPENSSL_SHA256}" "${_OPENSSL_URL_PREFIX}" || return 1
+	ensure_source_presence "${SRC_DIR}" "${_LIBCURL_ARCHIVE}" "${_LIBCURL_URL_PREFIX}" \
+	    "${_LIBCURL_SHA256}" "${_LIBCURL_SIG_SUFF}" "${_LIBCURL_KEY_FP}" || return 1
+	ensure_source_presence "${SRC_DIR}" "${_OPENSSL_ARCHIVE}" "${_OPENSSL_URL_PREFIX}" \
+	    "${_OPENSSL_SHA256}" "${_OPENSSL_SIG_SUFF}" "${_OPENSSL_KEY_FP}" || return 1
 
-	ensure_source_presence "${SRC_DIR}" "${_LIBISDS_ARCHIVE}" "${_LIBISDS_SHA256}" "${_LIBISDS_URL_PREFIX}" || return 1
+	ensure_source_presence "${SRC_DIR}" "${_LIBISDS_ARCHIVE}" "${_LIBISDS_URL_PREFIX}" \
+	    "${_LIBISDS_SHA256}" "${_LIBISDS_SIG_SUFF}" "${_LIBISDS_KEY_FP}" || return 1
 
 	return 0
 }
@@ -197,7 +320,7 @@ adjust_sources () {
 
 		_GETTEXT_ARCHIVE="gettext-0.19.7.tar.xz"
 		_GETTEXT_SHA256="378fa86a091cec3acdece3c961bb8d8c0689906287809a8daa79dc0c6398d934"
-		echo "Using ${_GETTEXT_ARCHIVE}."
+		echo "Using '${_GETTEXT_ARCHIVE}'."
 		;;
 	osx)
 		# libxml2 past version 2.9.2, which does compile, fail
@@ -209,7 +332,7 @@ adjust_sources () {
 
 		#_LIBXML2_ARCHIVE="libxml2-2.9.8.tar.gz"
 		#_LIBXML2_SHA256="0b74e51595654f958148759cfef0993114ddccccbb6f31aee018f3558e8e2732"
-		echo "Using ${_LIBXML2_ARCHIVE}"
+		echo "Using '${_LIBXML2_ARCHIVE}'."
 		;;
 	*)
 		echo "Using defaults."
