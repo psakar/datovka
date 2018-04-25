c790ad463f3abd891f7f35e07c5237e85c19bd66
Karel Slany <karel.slany@nic.cz>
2018-04-25 17:44:34 +0200

    Started writing tests for Isds::Message.

---
+++ tests/test_isds_message.cpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2014-2018 CZ.NIC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the
+ * OpenSSL library under certain conditions as described in each
+ * individual source file, and distribute linked combinations including
+ * the two.
+ */
+
+#include <QString>
+#include <QtTest/QtTest>
+
+#include "src/global.h"
+#include "src/isds/message_functions.h"
+#include "src/isds/message_interface.h"
+#include "src/log/log.h"
+#include "tests/test_isds_message.h"
+
+class TestIsdsMessage : public QObject {
+	Q_OBJECT
+
+public:
+	TestIsdsMessage(void);
+
+private slots:
+	void initTestCase(void);
+
+	void cleanupTestCase(void);
+
+	void nullResponse(void);
+
+	void loadZfo(void);
+
+private:
+	const QString msgPath;
+	const QString delInfoPath;
+};
+
+TestIsdsMessage::TestIsdsMessage(void)
+    : QObject(Q_NULLPTR),
+    msgPath("data/DZ_6452235.zfo"),
+    delInfoPath("data/DD_6452235.zfo")
+{
+}
+
+void TestIsdsMessage::initTestCase(void)
+{
+	QVERIFY(GlobInstcs::logPtr == Q_NULLPTR);
+	GlobInstcs::logPtr = new (std::nothrow) LogDevice;
+	QVERIFY(GlobInstcs::logPtr != Q_NULLPTR);
+}
+
+void TestIsdsMessage::nullResponse(void)
+{
+	Isds::Message message;
+	QVERIFY(message.isNull());
+
+	QVERIFY(message.raw().isNull());
+	QVERIFY(message.rawType() == Isds::Type::RT_UNKNOWN);
+	QVERIFY(message.envelope().isNull());
+	QVERIFY(message.documents().isEmpty());
+
+	QVERIFY(Isds::messageFromData(QByteArray(), Isds::LT_ANY).isNull());
+	QVERIFY(Isds::messageFromData(QByteArray(), Isds::LT_MESSAGE).isNull());
+	QVERIFY(Isds::messageFromData(QByteArray(), Isds::LT_DELIVERY).isNull());
+
+	QByteArray garbageData("abcdef01235");
+	QVERIFY(Isds::messageFromData(garbageData, Isds::LT_ANY).isNull());
+	QVERIFY(Isds::messageFromData(garbageData, Isds::LT_MESSAGE).isNull());
+	QVERIFY(Isds::messageFromData(garbageData, Isds::LT_DELIVERY).isNull());
+}
+
+void TestIsdsMessage::loadZfo(void)
+{
+	Isds::Message message01, message02;
+	QVERIFY(message01.isNull());
+	QVERIFY(message02.isNull());
+	QVERIFY(message01 == message01);
+	QVERIFY(message01 == message02);
+	QVERIFY(message02 == message02);
+
+	QVERIFY(!Isds::messageFromFile(msgPath, Isds::LT_ANY).isNull());
+	QVERIFY(!Isds::messageFromFile(msgPath, Isds::LT_MESSAGE).isNull());
+	QVERIFY(Isds::messageFromFile(msgPath, Isds::LT_DELIVERY).isNull());
+
+	QVERIFY(!Isds::messageFromFile(delInfoPath, Isds::LT_ANY).isNull());
+	QVERIFY(Isds::messageFromFile(delInfoPath, Isds::LT_MESSAGE).isNull());
+	QVERIFY(!Isds::messageFromFile(delInfoPath, Isds::LT_DELIVERY).isNull());
+}
+
+void TestIsdsMessage::cleanupTestCase(void)
+{
+	delete GlobInstcs::logPtr; GlobInstcs::logPtr = Q_NULLPTR;
+}
+
+QObject *newTestIsdsMessage(void)
+{
+	return new (std::nothrow) TestIsdsMessage();
+}
+
+//QTEST_MAIN(TestIsdsMessage)
+#include "test_isds_message.moc"
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -29,6 +29,7 @@
 #include "tests/test_db_container.h"
 #include "tests/test_db_single.h"
 #include "tests/test_isds_login.h"
+#include "tests/test_isds_message.h"
 #include "tests/test_message_db_set.h"
 #include "tests/test_task_downloads.h"
 #include "tests/test_task_send_message.h"
@@ -90,6 +91,10 @@ int main(int argc, char *argv[])
 #if defined TEST_TASK_DOWNLOADS
 	status |= testThisClassAndDelete(newTestTaskDownloads(msgId), argc, argv);
 #endif /* defined TEST_TASK_DOWNLOADS */
+
+#if defined TEST_ISDS_MESSAGE
+	status |= testThisClassAndDelete(newTestIsdsMessage(), argc, argv);
+#endif /* defined TEST_ISDS_MESSAGE */
 
 #if defined TEST_VERSION
 	status |= testThisClassAndDelete(newTestVersion(), argc, argv);
